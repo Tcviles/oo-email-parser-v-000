9ce01ef7fc6a93fab910cdc73caa0c3803f57a68
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-07-07 19:19:27 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,7 +9,8 @@ class EmailParser
   end
 
   def parse
-    emails_list = @emails.split(", ")
-    answer  = emails_list.uniq
+    emails_list = @emails.split(" ")
+    cleaned_emails_list = emails_list.collect {|email| email.chomp(",")}
+    answer  = cleaned_emails_list.uniq
   end
 end
